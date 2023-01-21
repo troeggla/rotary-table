1e6deeb32ae76b495f392e78bbee55cbc08a7009
Thomas Röggla <tom@lichtfragmente.com>
2023-01-21 06:19:47 +0100

    added reset flag to screen class

--- screen.h
+++ screen.h
@@ -8,10 +8,15 @@ class Screen {
 protected:
   U8G2& u8g2;
   Keypad& keypad;
+  bool reset = false;
 
 public:
   Screen(U8G2& u8g2, Keypad& keypad): u8g2(u8g2), keypad(keypad) {}
   virtual ~Screen() {}
+
+  bool isReset() {
+    return reset;
+  }
 
   virtual void draw() = 0;
   virtual String getName() = 0;
