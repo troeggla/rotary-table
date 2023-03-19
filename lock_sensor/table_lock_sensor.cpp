db84dae11ef533d29d91fa1b6a50c30174c4f600
Thomas Röggla <tom@lichtfragmente.com>
2023-03-19 18:13:31 +0100

    removed currentLedState member variable

--- lock_sensor/table_lock_sensor.cpp
+++ lock_sensor/table_lock_sensor.cpp
@@ -3,9 +3,6 @@
 TableLockSensor::TableLockSensor(uint8_t sensorPinNum, uint8_t ledPinNum, uint8_t lockedState) : sensorPinNum(sensorPinNum), ledPinNum(ledPinNum), lockedState(lockedState) {
   pinMode(sensorPinNum, INPUT_PULLUP);
   pinMode(ledPinNum, OUTPUT);
-
-  currentLedState = LOW;
-  digitalWrite(ledPinNum, currentLedState);
 }
 
 bool TableLockSensor::isLocked() {
@@ -14,12 +11,10 @@ bool TableLockSensor::isLocked() {
 
 void TableLockSensor::setLedState(uint8_t state) {
   digitalWrite(ledPinNum, state);
-  currentLedState = state;
 }
 
 void TableLockSensor::updateLockLed() {
-  currentLedState = (isLocked()) ? HIGH : LOW;
-  digitalWrite(ledPinNum, currentLedState);
+  digitalWrite(ledPinNum, (isLocked()) ? HIGH : LOW);
 }
 
 void TableLockSensor::flashLed() {
--- lock_sensor/table_lock_sensor.h
+++ lock_sensor/table_lock_sensor.h
@@ -9,8 +9,6 @@ class TableLockSensor {
   const uint8_t ledPinNum;
   const uint8_t lockedState;
 
-  uint8_t currentLedState;
-
 public:
   TableLockSensor(uint8_t sensorPinNum, uint8_t ledPinNum, uint8_t lockedState);
   TableLockSensor(uint8_t sensorPinNum, uint8_t ledPinNum) : TableLockSensor(sensorPinNum, ledPinNum, HIGH) {};
